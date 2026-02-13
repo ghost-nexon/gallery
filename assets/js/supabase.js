580c65f86a7c6a07e08a48a60e9664a0add327c3
mahiru <aicrea.t.i.o.n.sab.h.i@gmail.com>
2026-02-13 10:33:46 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'https://your-supabase-project.supabase.co'; // Replace with your Supabase URL
-const supabaseAnonKey = 'your-anon-key'; // Replace with your Supabase Anon Key
+const supabaseUrl = 'https://oolcpfghhfmxidtmmsch.supabase.co'; // Replace with your Supabase URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbGNwZmdoaGZteGlkdG1tc2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODI0NTMsImV4cCI6MjA4NjQ1ODQ1M30.rcA6myxbCe_FjOHRfYRBJUch4nOwcjpMlEvVLLNUlt4'; // Replace with your Supabase Anon Key
 
 const { createClient } = supabase;
 window.supabase = createClient(supabaseUrl, supabaseAnonKey);
