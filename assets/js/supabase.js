83e058ae44e9ccdfd90a88a114ac8e7140de20d3
mahiru <aicrea.t.i.o.n.sab.h.i@gmail.com>
2026-02-13 16:19:46 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,6 @@
-const supabaseUrl = 'https://oolcpfghhfmxidtmmsch.supabase.co'; // Replace with your Supabase URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbGNwZmdoaGZteGlkdG1tc2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODI0NTMsImV4cCI6MjA4NjQ1ODQ1M30.rcA6myxbCe_FjOHRfYRBJUch4nOwcjpMlEvVLLNUlt4'; // Replace with your Supabase Anon Key
+// assets/js/supabase.js
+const supabaseUrl    = 'https://oolcpfghhfmxidtmmsch.supabase.co'
+const supabaseKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbGNwZmdoaGZteGlkdG1tc2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODI0NTMsImV4cCI6MjA4NjQ'
+const supabase       = window.supabase.createClient(supabaseUrl, supabaseKey)
 
-const { createClient } = supabase;
-window.supabase = createClient(supabaseUrl, supabaseAnonKey);
+window.supabase = supabase
