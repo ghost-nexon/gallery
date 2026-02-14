6366538d8de8f71829a35b9484ac949bf120bd68
mahiru <aicrea.t.i.o.n.sab.h.i@gmail.com>
2026-02-14 15:25:27 +0530

    Delete assets directory

--- assets/js/supabase.js
+++
@@ -1,6 +0,0 @@
-// assets/js/supabase.js
-const supabaseUrl    = 'https://oolcpfghhfmxidtmmsch.supabase.co'
-const supabaseKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vbGNwZmdoaGZteGlkdG1tc2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODI0NTMsImV4cCI6MjA4NjQ'
-const supabase       = window.supabase.createClient(supabaseUrl, supabaseKey)
-
-window.supabase = supabase
